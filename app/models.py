b1fcd271e30c9d6867698bc13a535a6b07ce1b1e
prplkane <kantemirmuratov@gmail.com>
2025-07-13 13:21:20 +0600

    Added new tables for fin data and backed up alembic

--- app/models.py
+++ app/models.py
@@ -1,5 +1,22 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, date
 from sqlmodel import SQLModel, Field, Relationship
+from enum import Enum
+from typing import Optional
+
+class PaymentStatus(str, Enum):
+    unpaid = 'unpaid'
+    partial = 'partial'
+    paid = 'paid'
+    overdue = 'overdue'
+    refunded = 'refunded'
+
+class Procedure(str, Enum):
+    cleaning = 'cleaning'
+    filling = 'filling'
+    extraction = 'extraction'
+    root_canal = 'root_canal'
+    crown = 'crown'
+
 
 class Client(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
@@ -21,9 +38,30 @@ class Deal(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     client_id: int= Field(foreign_key="client.id")
     amount: float
-    discount: float | None = Field(default=0.0, nullable=True)
+
+    category: Procedure
+
+    amount_gross: float
+    discount: float = Field(default=0.0, nullable=True)
+    tax_rate: float = Field(default=0.0, nullable=True)
+    tax_amount: float = Field(default=0.0, nullable=True)
+    amount_net: float 
     
-    status: str = Field(default='open', index=True)
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    client: Client | None = Relationship(back_populates="deals")
-    notes: str | None = Field(default=None, nullable=True) 
+    currency: str = Field(default='KGS')
+
+    invoice_number: Optional[str] = Field(default=None, nullable=True)
+    invoice_date:    datetime      = Field(default_factory=lambda: datetime.now(timezone.utc))
+    due_date:        Optional[date]    = Field(default=None, nullable=True)
+    paid_date:       Optional[datetime]= Field(default=None, nullable=True)
+
+    payment_status:  PaymentStatus  = Field(default=PaymentStatus.unpaid)
+    paid_amount:     Optional[float]  = Field(default=None, nullable=True)
+    payment_method:  Optional[str]    = Field(default=None, nullable=True)
+
+    status:  str   = Field(default="open", index=True) # THIS IS THE ONE THAT WAS VARCHAR AND CHANGED TO ENUM
+    notes:   Optional[str] = Field(default=None, nullable=True)
+
+    created_at: datetime            = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updated_at: Optional[datetime] = Field(default=None, nullable=True)
+
+    client: Optional[Client] = Relationship(back_populates="deals")
--- app/schemas.py
+++ app/schemas.py
@@ -1,45 +1,71 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
-from datetime import datetime
+from datetime import datetime, date
 from typing import Optional
+from app.models import Procedure, PaymentStatus
 
 
 #Client models
 class ClientCreate(BaseModel):
     first_name: str
-    last_name: str
     middle_name: Optional[str] = None
-    phone: str
+    last_name: str
     email: Optional[EmailStr] = None
+    phone: Optional[str] = None
     address: Optional[str] = None
     
 
 class ClientRead(BaseModel):
-    id:int
+    id: int
     first_name: str
-    last_name: str
     middle_name: Optional[str] = None
-    phone: str
+    last_name: str
     email: Optional[EmailStr] = None
+    phone: Optional[str] = None
     address: Optional[str] = None
+    is_active: bool
     created_at: datetime
-    is_active: bool = True
 
-    
     model_config = ConfigDict(from_attributes=True)
 
 #Deals model
 class DealCreate(BaseModel):
     client_id: int
-    amount: float
-    status: Optional[str] = 'open'
+    category: Procedure
+    amount_gross: float
+    discount: Optional[float] = 0.0
+    tax_rate: Optional[float] = 0.0
+
+    invoice_number: Optional[str] = None
+    invoice_date: Optional[datetime] = None
+    due_date: Optional[date] = None
+    payment_method: Optional[str] = None
+
     notes: Optional[str] = None
+
 
 class DealRead(BaseModel):
     id: int
     client_id: int
-    amount: float
+    category: Procedure
+
+    amount_gross: float
+    discount: float
+    tax_rate: float
+    tax_amount: float
+    amount_net: float
+    currency: str
+
+    invoice_number: Optional[str]
+    invoice_date: datetime
+    due_date: Optional[date]
+    paid_date: Optional[datetime]
+    payment_status: PaymentStatus
+    paid_amount: Optional[float]
+    payment_method: Optional[str]
+
     status: str
-    notes: Optional[str] = None
-    created_at: datetime    
+    notes: Optional[str]
+    created_at: datetime
+    updated_at: Optional[datetime]
 
     model_config = ConfigDict(from_attributes=True)
